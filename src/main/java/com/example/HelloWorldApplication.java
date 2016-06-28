57e4a653b2b1b09d6db71180b418e4624bc054b1
Ibrahim1414 <owf@windowslive.com>
2016-06-28 15:48:39 +0300

    Update HelloWorldApplication.java

--- src/main/java/com/example/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -1,22 +1,17 @@
 package com.example;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloWorldApplication {
-	
+public class GreetingController {
 
-@RequestMapping(value="", method= RequestMethod.GET)
-public String HelloWorld(ModelMap model) {
-	
-	return "HelloWorld";
-	
-}
-	
-}
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 
+}
